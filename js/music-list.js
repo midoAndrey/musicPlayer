af2099fff9313a2fbac0875249c65b1aca1702b9
midoAndrey <93534238+midoAndrey@users.noreply.github.com>
2023-07-11 13:40:28 +0300

    Add files via upload

---
+++ js/music-list.js
@@ -0,0 +1,38 @@
+let allMusic = [
+    {
+        name: "Alors on Dance",
+        artist: "Stromae",
+        img: "1",
+        src: "1-music"
+    },
+    {
+        name: "Living life",
+        artist: "Cheriimoya",
+        img: "2",
+        src: "2-music"
+    },
+    {
+        name: "Young",
+        artist: "Holywood Undead",
+        img: "3",
+        src: "3-music"
+    },
+    {
+        name: "Metamorphosis",
+        artist: "Interworld",
+        img: "4",
+        src: "4-music"
+    },
+    {
+        name: "Do Ya Thing",
+        artist: "P$C feat. Young Dro",
+        img: "5",
+        src: "5-music"
+    },
+    {
+        name: "The Less I Know The Better",
+        artist: "Tame Impala",
+        img: "6",
+        src: "6-music"
+    }
+]
